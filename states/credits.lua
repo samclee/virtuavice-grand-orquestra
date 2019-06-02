52caac259ff6239d4c21f93e0ea6f1da396427ad
samclee <samclee@gmail.com>
2019-06-02 00:36:19 -0700

    Added boss portrait

--- states/credits.lua
+++ states/credits.lua
@@ -1,26 +1,59 @@
 local Credits = {}
 
-local ppl ={
+local y = 610
+local lerp_started = false
+local cr =[[
+  Story and Writing
+  Grace Wilhoite
+
+  Programming
+  Sam Lee
+
+  Art
+  Angie Ni
+  Kristen Shih
+  Angie Ta
+  Grace Wilhoite
+
+  Music
+  Kyle Chuang
+  Savita Pereira
+
+
+
   
-}
+
+
+
+
+  Thank you for playing!
+]]
 
 function Credits:enter(from)
   print('entered credits')
-end
-
-function Credits:resume(from)
-
+  lg.setFont(fonts.dialog)
+  s:reset()
 end
 
 function Credits:update(dt)
-
+  s:update(dt)
+  if y > -968 then
+    y = y - .5
+  end
 end
 
 function Credits:draw()
   s:on()
-  lg.print('credits',0,0)
+  lg.setColor(1,1,1)
+  lg.rectangle('fill',0,0,800,600)
 
+  lg.setColor(0,0,0)
+  lg.printf(cr, 100,y,600, 'center')
   s:off()
+end
+
+function Credits:keypressed(k)
+  --s:fadeTo({1,0,0,0},2)
 end
 
 return Credits
